319dc911fb06a04886693e6ce5d5da27b0db605d
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2022-03-25 13:54:42 +0100

    Update start-clickhouse.sh

--- start-clickhouse.sh
+++ start-clickhouse.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# Copy UDFs Binaries to:
+#    /var/lib/clickhouse/user_scripts/
+# Copy UDF Scripts to:
+#    /etc/clickhouse-server/*_function.xml
+
 echo "Starting ClickHouse"
-docker run --name clickhouse -p 9000:9000 -p 8123:8123 -d yandex/clickhouse-server
+docker run --name clickhouse -p 9000:9000 -p 8123:8123 -d clickhouse/clickhouse-server:latest-alpine
