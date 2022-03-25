c6314aac0bc73f78368dfb4def3a935e620f1182
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2022-03-25 14:19:49 +0100

    Update start-clickhouse.sh

--- start-clickhouse.sh
+++ start-clickhouse.sh
@@ -4,6 +4,8 @@
 #    /var/lib/clickhouse/user_scripts/
 # Copy UDF Scripts to:
 #    /etc/clickhouse-server/*_function.xml
+# Mount loader script.sh to:
+#    /docker-entrypoint-initdb.d/
 
 echo "Starting ClickHouse"
-docker run --name clickhouse -p 9000:9000 -p 8123:8123 -d clickhouse/clickhouse-server:latest-alpine
+docker run --name clickhouse -p 9000:9000 -p 8123:8123 -v "${{ github.workspace }}":"/workspace" -d clickhouse/clickhouse-server:latest-alpine
